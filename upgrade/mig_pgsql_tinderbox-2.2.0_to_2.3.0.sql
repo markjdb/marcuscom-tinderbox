7768ce387d6fa70fc6ac8c1f3067d728dec1ca5a
marcus <marcus>
2006-01-23 18:37:22 +0000

    Add a missing ';' to the end of a query.
    Reported by:	lesi

--- upgrade/mig_pgsql_tinderbox-2.2.0_to_2.3.0.sql
+++ upgrade/mig_pgsql_tinderbox-2.2.0_to_2.3.0.sql
@@ -1,4 +1,4 @@
 ALTER TABLE builds
-  ADD COLUMN Build_Last_Updated TIMESTAMP
+  ADD COLUMN Build_Last_Updated TIMESTAMP;
 
 UPDATE config SET Config_Option_Value='2.3.0' WHERE Config_Option_Name='__DSVERSION__';
