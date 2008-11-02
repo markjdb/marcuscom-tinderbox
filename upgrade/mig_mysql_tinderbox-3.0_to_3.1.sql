74fa8d0a5d1a501637bc46310b27d033cf582845
marcus <marcus>
2008-11-02 15:47:16 +0000

    Bump __DSVERSION__ to 3.1.

--- upgrade/mig_mysql_tinderbox-3.0_to_3.1.sql
+++ upgrade/mig_mysql_tinderbox-3.0_to_3.1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE user_permissions DROP PRIMARY KEY;
 ALTER TABLE user_permissions ADD PRIMARY KEY (user_id, user_permission_object_type, user_permission_object_id, user_permission);
+UPDATE config SET config_option_value = '3.1' WHERE config_option_name = '__DSVERSION__';
--- upgrade/mig_pgsql_tinderbox-3.0_to_3.1.sql
+++ upgrade/mig_pgsql_tinderbox-3.0_to_3.1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE user_permissions DROP CONSTRAINT user_permissions_pkey;
 ALTER TABLE user_permissions ADD CONSTRAINT user_permissions_pkey PRIMARY KEY (user_id, user_permission_object_type, user_permission_object_id, user_permission);
+UPDATE config SET config_option_value = '3.1' WHERE config_option_name = '__DSVERSION__';
