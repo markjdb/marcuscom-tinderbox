e9911d0b8b4dc8efcf5fb2e27c1a452d1c067a61
marcus <marcus>
2008-11-03 18:22:31 +0000

    Fix the upgrade path.  Man, it's been a long time since I did a minor update.

--- lib/tc_command.sh
+++ lib/tc_command.sh
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/tc_command.sh,v 1.101.2.6 2008/11/02 16:13:29 marcus Exp $
+# $MCom: portstools/tinderbox/lib/tc_command.sh,v 1.101.2.7 2008/11/03 18:22:31 marcus Exp $
 #
 
 export _defaultUpdateHost="cvsup12.FreeBSD.org"
@@ -288,9 +288,9 @@ Setup () {
 #---------------------------------------------------------------------------
 
 Upgrade () {
-    VERSION="3.0"
+    VERSION="3.1"
     TINDERBOX_URL="http://tinderbox.marcuscom.com/"
-    DB_MIGRATION_PATH="${VERSION} 3.1"
+    DB_MIGRATION_PATH="3.0 ${VERSION}"
 
     bkup_file=""
 
