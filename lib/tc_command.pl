749ed8b447e97d1fb0e7bf3df5d069688d3fd3cd
marcus <marcus>
2009-03-27 04:16:27 +0000

    MFH:
    Correct a typo which broken copyBuild for packages.

--- lib/tc_command.pl
+++ lib/tc_command.pl
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/tc_command.pl,v 1.150.2.12 2009/02/11 08:50:34 beat Exp $
+# $MCom: portstools/tinderbox/lib/tc_command.pl,v 1.150.2.13 2009/03/27 04:16:27 marcus Exp $
 #
 
 my $pb;
@@ -3090,7 +3090,7 @@ sub copyBuildPorts {
                         }
 
                         $rc =
-                            $ds->updatePortLastBuiltStatus($port, $dest,
+                            $ds->updatePortLastStatus($port, $dest,
                                 $ds->getPortLastBuiltStatus($port, $src));
                         if (!$rc) {
                                 warn
