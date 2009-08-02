07471254e0c8d05c9749303588983e978a102892
marcus <marcus>
2009-08-02 17:51:42 +0000

    MFH:
    We can't use -delete with find when following symlinks.  Instead, use
    xargs to remove the broken links.

--- lib/tc_command.sh
+++ lib/tc_command.sh
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/lib/tc_command.sh,v 1.101.2.23 2009/07/18 21:22:43 marcus Exp $
+# $MCom: portstools/tinderbox/lib/tc_command.sh,v 1.101.2.24 2009/08/02 17:51:42 marcus Exp $
 #
 
 export _defaultUpdateHost="cvsup17.FreeBSD.org"
@@ -2210,7 +2210,7 @@ tbcleanup () {
 	        done
 
 		echo "Pruning broken package symlinks for build ${build}"
-		find -L ${pkg_path} -type l -delete
+		find -L ${pkg_path} -type l | xargs rm -f
         fi
 
         tbcleanup_cleanup ${portstree}
