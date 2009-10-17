d4e7aa560f776c2ac11d20c986aa7e06868de006
marcus <marcus>
2009-10-17 19:54:30 +0000

    Add upgrade code for __DSVERSION__ 3.2.1.

---
+++ upgrade/mig_mysql_tinderbox-3.2_to_3.2.1.sql
@@ -0,0 +1,3 @@
+INSERT INTO config VALUES ('LOG_DIRECTORY', '');
+INSERT INTO config VALUES ('LOG_DOCOPY', '0');
+UPDATE config SET config_option_value = '3.2.1' WHERE config_option_name = '__DSVERSION__';
---
+++ upgrade/mig_pgsql_tinderbox-3.2_to_3.2.1.sql
@@ -0,0 +1,4 @@
+INSERT INTO config VALUES ('LOG_DIRECTORY', '');
+INSERT INTO config VALUES ('LOG_DOCOPY', '0');
+UPDATE config SET config_option_value = '3.2.1' WHERE config_option_name = '__DS
+VERSION__';
