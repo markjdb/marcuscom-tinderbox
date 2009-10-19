295f2b1523351543d9b2d5756872244dfd2e93aa
beat <beat>
2009-10-19 19:09:22 +0000

    MFH:
    Teach webui that domain part of maintainer address is in lowercase now.
    
    To update the existing data (works with mySQL and PostgreSQL):
    
    UPDATE ports
    SET port_maintainer=REPLACE(port_maintainer, '@FreeBSD.org','@freebsd.org');

--- webui/core/TinderboxDS.php
+++ webui/core/TinderboxDS.php
@@ -24,7 +24,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $MCom: portstools/tinderbox/webui/core/TinderboxDS.php,v 1.36.2.13 2009/10/13 20:44:25 beat Exp $
+# $MCom: portstools/tinderbox/webui/core/TinderboxDS.php,v 1.36.2.14 2009/10/19 19:09:22 beat Exp $
 #
 
     require_once 'MDB2.php';
@@ -386,7 +386,7 @@
 	    }
             $query = "SELECT p.port_id,
                              p.port_directory,
-                             REPLACE(p.port_maintainer, '@FreeBSD.org', '') as port_maintainer,
+                             REPLACE(p.port_maintainer, '@freebsd.org', '') as port_maintainer,
                              p.port_name,
                              p.port_comment,
                              bp.last_built,
